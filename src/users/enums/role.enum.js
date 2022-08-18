469023c849871702638172c3807c54e7662665f8
cookapps-jhlee11 <jhlee11@cookapps.com>
2022-08-18 11:11:02 +0900

    fix: Swagger build action debug ( Added 'services' )

--- src/users/enums/role.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["standard"] = "standard";
-    Role["premium"] = "premium";
-})(Role = exports.Role || (exports.Role = {}));
