aa35f5a87155d7ab26e0911ec0cc576add8b76a8
cookapps-jhlee11 <jhlee11@cookapps.com>
2022-08-23 11:16:19 +0900

    chore: manifest 내 metadata 이름 통일

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,6 @@ let server: Handler;
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const config = new DocumentBuilder()
-    .setTitle('Nestjs Boilerplate App')
-    .setDescription('The N.B.As API Description')
-    .setVersion('1.0')
-    .addTag('Users')
-    .addServer('http://localhost:3001',"로컬 호스트 서버")
-    .addServer('http://1.2.3.4:3000',"Dev 서버")
-    .addServer('https://tech.blablabla.blabla/apis',"prod 서버")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app,document);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
